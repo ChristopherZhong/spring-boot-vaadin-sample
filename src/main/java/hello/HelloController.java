4e6e50997d448e02f9fc4f02c74dd4fbb06524dd
Christopher Zhong <chris.zhong@yantramail.com>
2014-03-19 13:50:04 -0500

    removed external properties file location and added test message that can be shown using the rest url

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-@PropertySource(value = { "file:${SPRING_CONFIG_LOCATION}" })
 public class HelloController {
 
     @Value(value = "test.message")
