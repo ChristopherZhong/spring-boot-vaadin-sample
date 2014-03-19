40797a71578b746517f6f31aa1c61a54b35ef1d2
Christopher Zhong <chris.zhong@yantramail.com>
2014-03-19 17:40:13 -0500

    added missing component scan annotation

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@ComponentScan
 public class HelloController {
 
     @Value(value = "test.message")
